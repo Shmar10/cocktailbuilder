aad1a0a5171d3c6dde34e0e66175fbeba2ff631e
Shmar10 <shamar@d219.org>
2025-08-25 12:57:51 -0500

    Upsert recipe: Paper Plane

--- custon-recipes.js
+++ custon-recipes.js
@@ -32,5 +32,42 @@
     "glass": "Rocks",
     "garnish": "Lemon wheel",
     "tags": []
+  },
+  {
+    "id": "paper-plane",
+    "name": "Paper Plane",
+    "base": [
+      "bourbon"
+    ],
+    "profile": [
+      "citrusy",
+      "bitter"
+    ],
+    "sweetness": "balanced",
+    "ingredients": [
+      [
+        "0.75 oz",
+        "Bourbon"
+      ],
+      [
+        "0.75 oz",
+        "Aperol"
+      ],
+      [
+        "0.75 oz",
+        "Amaro Nonino"
+      ],
+      [
+        "0.75 oz",
+        "Lemon juice"
+      ]
+    ],
+    "method": "Shake with ice, strain into coupe.",
+    "glass": "Coupe",
+    "garnish": "Lemon twist",
+    "tags": [
+      "shaken",
+      "modern classic"
+    ]
   }
 ]
