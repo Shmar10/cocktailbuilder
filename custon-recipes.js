33ef49914bc00a27aec109aca2a91d01422b0817
Shmar10 <shamar@d219.org>
2025-08-25 11:26:17 -0500

    Add recipe: Arctic Goldrush

--- custon-recipes.js
+++ custon-recipes.js
@@ -1,1 +1,36 @@
-[]
+[
+  {
+    "id": "arctic-goldrush",
+    "name": "Arctic Goldrush",
+    "base": [
+      "bourbon",
+      "kronan swedish punsch"
+    ],
+    "profile": [
+      "citrusy"
+    ],
+    "sweetness": "balanced",
+    "ingredients": [
+      [
+        "2 oz",
+        "Bourbon"
+      ],
+      [
+        "3/4 oz",
+        "Kronan Swedish Punsch"
+      ],
+      [
+        "3/4 oz",
+        "Fresh Lemon Juice"
+      ],
+      [
+        "1/4 oz",
+        "Honey Syrup (1:1 honey to water)"
+      ]
+    ],
+    "method": "Shake all ingredients with ice, strain into rocks glass over fresh ice. Garnish with lemon wheel.",
+    "glass": "Rocks",
+    "garnish": "Lemon wheel",
+    "tags": []
+  }
+]
